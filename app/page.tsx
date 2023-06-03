0b1061223dfeeacf6bb4963c4ce0e5fcb116e01c
jun094 <jun094@naver.com>
2023-06-04 01:40:05 +0900

    feat: ssg 페이지 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { API_URL, CONTENTS_PATH } from '@/constants';
+
+const fetchContents = async () => {
+  const res = await fetch(`${API_URL}${CONTENTS_PATH}`, {
+    next: { revalidate: 10 },
+  });
+  const contents: any = await res.json();
+  return contents;
+};
+
+export default async function Home() {
+  const contents = await fetchContents();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home page
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="mb-8 text-5xl">home page</h1>
+
+      {contents.map(({ path, name }: any) => (
+        <h3 key={path} className="text-2xl">
+          {name}
+        </h3>
+      ))}
     </main>
   );
 }
---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const CONTENTS_PATH = '/creators';
+
+export const API_URL =
+  'https://api.github.com/repos/meetup-fe-next/review-contents/contents';
