0683d3d4d1f6a6bb4d21ea2def1fa681f460d44d
Jeesoo Min <kuongee@gmail.com>
2023-06-14 21:50:05 +0900

    feat: Icon 컴포넌트 추가

---
+++ components/Icon/index.tsx
@@ -0,0 +1,11 @@
+import ArrowLeft from './ArrowLeft';
+import Profile from './Profile';
+import Search from './Search';
+
+export type IconProps = {
+  width: number;
+  height: number;
+  color: string;
+};
+
+export { ArrowLeft, Profile, Search };
