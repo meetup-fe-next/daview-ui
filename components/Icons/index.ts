401d39b5e19888cafa32fcccd6faee9c2a3cafad
Jeesoo Min <kuongee@gmail.com>
2023-06-18 23:08:54 +0900

    feat: 화살표, 검색 아이콘 추가

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,6 +1,13 @@
 import ArrowLeft from './ArrowLeft';
+<<<<<<< HEAD
 import Heart from './Heart';
 import Profile from './Profile';
 import Search from './Search';
 
 export { ArrowLeft, Heart, Profile, Search };
+=======
+import Profile from './Profile';
+import Search from './Search';
+
+export { ArrowLeft, Profile, Search };
+>>>>>>> 604c163 (feat: 화살표, 검색 아이콘 추가)
