1624abc004fee38e790a89ea7d2f1665204f5c57
jun094 <jun094@naver.com>
2023-06-27 01:11:21 +0900

    feat: alogolia 테스트 컴포넌트 렌더링

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import SearchContainer from '@/ui/mainPage/SearchContainer';
+
 function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="mb-8 text-5xl">home page</h1>
+
+      <SearchContainer />
     </main>
   );
 }
--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,7 @@ export const GITHUB_REPOSITORY_NAME = 'review-contents';
 export const API_URL = `https://api.github.com/repos/${GITHUB_USER_NAME}/${GITHUB_REPOSITORY_NAME}/contents`;
 
 export const REVALIDATE_GITHUB_DATA = 60 * 60; // 1시간
+
+export const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string;
+
+export const ALGOLIA_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_API_KEY as string;
