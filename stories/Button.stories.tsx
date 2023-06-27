a0c3581829f17b2af9b8b68c41d3d3122f7a0ffb
Wonyoung Yoo <yooveloper@gmail.com>
2023-06-27 14:54:54 +0900

    feat: setup storybook (#12)
    * feat: storybook 관련 의존성 추가
    
    * feat: storybook 관련 설정
    
    * feat: Button component story sample
    
    * feat:headline 추가
    
    * Feat: typography headline 추가 (#5)
    
    * feat:typography headline 추가
    
    * feat:typography headline 추가
    
    * feat:typography headline review 내용 추가
    
    * feat: Icon 컴포넌트 추가 (#10)
    
    ---------
    
    Co-authored-by: jinyoung <danroom85@gmail.com>
    Co-authored-by: plagdoctor <42507121+plagdoctor@users.noreply.github.com>
    Co-authored-by: Jeesoo Min <34534423+kuongee@users.noreply.github.com>

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,12 @@
+import Button from '../components/Button';
+
+export default {
+  title: 'components/Button',
+  component: Button,
+};
+
+export const Primary = () => <Button color="primary">Primary</Button>;
+
+export const Small = () => <Button size="sm">Small</Button>;
+
+export const Outline = () => <Button shape="outline">Outline</Button>;
