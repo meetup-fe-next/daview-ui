94e72121dd78d6fb574ba42be228b47d92fbd9d4
Jeesoo Min <kuongee@gmail.com>
2023-06-28 00:15:47 +0900

    feat: 디자인 반영

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -2,7 +2,7 @@
 
 import cn from 'classnames';
 import { useRef, useState } from 'react';
-import { CloseCircle, Search } from '@/components/Icons';
+import { ArrowLeft, CloseCircle, Search } from '@/components/Icons';
 
 export type InputProps = {
   disabled?: boolean;
@@ -10,28 +10,77 @@ export type InputProps = {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onClickClear?: () => void;
+  onClickPreviousButton?: () => void;
+  onClickSearchButton?: () => void;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ disabled, placeholder, value = '', onChange, onClickClear, ...rest }: InputProps) => {
+const Input = ({
+  disabled,
+  placeholder,
+  value = '',
+  onChange,
+  onClickClear,
+  onClickPreviousButton,
+  onClickSearchButton,
+  ...rest
+}: InputProps) => {
   const ref = useRef<HTMLInputElement>(null);
   const [inputValue, setValue] = useState(value);
   const [isFocus, setIsFocus] = useState(false);
 
+  const isDefaultType = () => {
+    return !isFocus && inputValue.length === 0;
+    // return true;
+  };
+
+  const isActiveType = () => {
+    return isFocus;
+  };
+
+  const isValueType = () => {
+    return inputValue.length > 0;
+    // return true;
+  };
+
   return (
-    <label className="relative block w-[343px]">
+    <label className="relative block w-full">
       <span className="sr-only">Search</span>
-      {!isFocus && (
+
+      {/* 좌측 버튼 */}
+      {isDefaultType() && (
         <span className="absolute inset-y-0 left-0 flex items-center pl-[8px]">
-          <Search className={cn(disabled && 'opacity-[0.32]')} color="black" width={24} height={24} />
+          <Search
+            className={cn(disabled && 'opacity-[0.32]')}
+            color=""
+            strokeColor="secondary-900"
+            width={24}
+            height={24}
+          />
         </span>
       )}
+      {isActiveType() && (
+        <button className="absolute inset-y-0 left-0 flex items-center pl-[8px]">
+          <ArrowLeft
+            color="black"
+            width={24}
+            height={24}
+            onMouseDown={() => {
+              onClickPreviousButton && onClickPreviousButton();
+            }}
+          />
+        </button>
+      )}
+
       <input
         ref={ref}
         className={cn(
-          'box-border	border-b-2 border-secondary-900 bg-transparent text-[20px] outline-none focus:border-b-primary-500',
+          'box-border border-b-2 border-secondary-900 bg-transparent text-[20px] outline-none focus:border-b-primary-500',
           'placeholder:text-secondary-600',
-          'w-full py-[8px] pl-[42px] pr-[15px] focus:-z-10 focus:pl-[8px]',
+          'w-full py-[8px]',
           'disabled:opacity-[.32]',
+          { 'pl-[42px] pr-[15px]': isDefaultType() || isActiveType() },
+          { 'pr-[76px]': isValueType() },
+          { 'pl-[8px]': !isActiveType() && isValueType() },
         )}
         type="text"
         disabled={disabled}
@@ -49,15 +98,28 @@ const Input = ({ disabled, placeholder, value = '', onChange, onClickClear, ...r
         }}
         {...rest}
       />
-      {isFocus && inputValue.length > 0 && (
-        <button
-          className="absolute inset-y-0 right-0 z-10 flex items-center pr-[8px]"
-          onMouseDown={() => {
-            onClickClear && onClickClear();
-          }}
-        >
-          <CloseCircle color="black" width={24} height={24} />
-        </button>
+      {isValueType() && (
+        <>
+          <button
+            className="absolute inset-y-0 right-0 flex items-center pr-[42px]"
+            onMouseDown={(e) => {
+              e.stopPropagation();
+              setValue('');
+              onClickClear && onClickClear();
+            }}
+          >
+            <CloseCircle color="grey-700" width={24} height={24} />
+          </button>
+          <button
+            className="absolute inset-y-0 right-0 flex items-center pr-[8px]"
+            onMouseDown={(e) => {
+              e.stopPropagation();
+              onClickSearchButton && onClickSearchButton();
+            }}
+          >
+            <Search width={24} height={24} color="" strokeColor={!isFocus ? 'secondary-900' : 'primary-500'} />
+          </button>
+        </>
       )}
     </label>
   );
