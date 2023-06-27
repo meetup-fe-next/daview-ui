8f3f9f527e793f90d0561aadef4ded5c784828d1
jun094 <jun094@naver.com>
2023-06-28 01:49:29 +0900

    test: qa 코드

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,37 @@
 import { Heart, ArrowLeft, Search, Profile } from '@/components/Icons';
+import PageLayout from '@/components/PageLayout';
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="mb-8 text-5xl">home page</h1>
-
-      {/* TODO: 스토리북으로 대체 */}
-      <Heart color="primary-300" />
-      <ArrowLeft size="lg" />
-      <Search color="black" width={50} height={50} />
-      <Profile size="sm" />
-    </main>
+    <PageLayout.Contents>
+      <ul>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+        <li className="text-2xl">list</li>
+      </ul>
+    </PageLayout.Contents>
   );
 }
