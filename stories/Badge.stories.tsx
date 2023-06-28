7ac61daa7b04ffe827ce277716c7d616007b0c7b
yooveloper <yooveloper@gmail.com>
2023-06-28 19:14:19 +0900

    test: add Badge story

---
+++ stories/Badge.stories.tsx
@@ -0,0 +1,14 @@
+import Badge from '../components/Badge';
+
+export default {
+  title: 'components/Badge',
+  component: Badge,
+};
+
+export const Primary = () => <Badge color="primary">Badge</Badge>;
+
+export const Secondary = () => <Badge color="secondary">Badge</Badge>;
+
+export const Blue = () => <Badge color="blue">Badge</Badge>;
+
+export const Gray = () => <Badge color="gray">Badge</Badge>;
