05dc69d464a6e669c367d082d707c7673c5b2cd4
yooveloper <yooveloper@gmail.com>
2023-06-29 14:44:31 +0900

    test: leftIcon story 추가

--- stories/Badge.stories.tsx
+++ stories/Badge.stories.tsx
@@ -1,9 +1,12 @@
-import Badge from '../components/Badge';
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import Badge, { BadgeProps } from '../components/Badge';
+import ExclamationMark from '../components/Icons/ExclamationMark';
 
 export default {
   title: 'components/Badge',
   component: Badge,
-};
+} as Meta;
 
 export const Primary = () => <Badge color="primary">Badge</Badge>;
 
@@ -12,3 +15,15 @@ export const Secondary = () => <Badge color="secondary">Badge</Badge>;
 export const Blue = () => <Badge color="blue">Badge</Badge>;
 
 export const Gray = () => <Badge color="gray">Badge</Badge>;
+
+type Story = StoryObj<typeof Badge>;
+
+export const LeftIconWithPrimary: Story = {
+  render: (args) => <Badge {...args} />,
+  args: {
+    color: 'primary',
+    children: 'Badge',
+    type: 'leftIcon',
+    leftIcon: <ExclamationMark width={14} height={14} />,
+  },
+};
