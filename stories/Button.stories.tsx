eb01bed27c9247a0c394eed153504a815f0aa10a
Jeesoo Min <kuongee@gmail.com>
2023-06-29 22:50:03 +0900

    feat: 버튼 스토리 추가

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,12 +1,40 @@
-import Button from '@/components/Button';
+import type { StoryObj } from '@storybook/react';
+import Button from '../components/Button';
+import { Heart } from '../components/Icons';
 
 export default {
   title: 'components/Button',
   component: Button,
 };
 
-export const Primary = () => <Button color="primary">Primary</Button>;
+type Story = StoryObj<typeof Button>;
 
-export const Small = () => <Button size="sm">Small</Button>;
-
-export const Outline = () => <Button shape="outline">Outline</Button>;
+export const Text: Story = {
+  render: (args) => (
+    <div className='flex gap-[20px]'>
+      <Button {...args}></Button>
+      <Button {...args}>
+        <Heart />
+        {args.children}
+      </Button>
+      <Button {...args}>
+        {args.children}
+        <Heart />
+      </Button>
+      <Button {...args}>
+        <Heart />
+      </Button>
+    </div>
+  ),
+  argTypes: {
+    onClick: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  args: {
+    disabled: false,
+    children: 'BUTTON',
+  },
+};
