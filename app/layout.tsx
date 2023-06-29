9dec2fcc760721653bf620ab7725df60e9d4abe6
jun094 <jun094@naver.com>
2023-06-30 02:10:06 +0900

    docse: 컴포넌트 사용법 주석 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body>
+        {/**
+         * NOTE: PageLayout 컴포넌트를 layout에 감싸주어 모든 페이지 컴포넌트에 적용될 수 있도록 해주었습니다.
+         * - PageLayout.TopFixed 안에 들어갈 요소들(Header, Input)이 페이지 별로 상이하여,
+         * - PageLayout.TopFixed 컴포넌트는 각 page.tsx에서 선언할 수 있도록 했습니다.
+         */}
         <PageLayout>{children}</PageLayout>
       </body>
     </html>
