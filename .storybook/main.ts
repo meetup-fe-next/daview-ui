8046b7c88f4f1e3e4366c79e00965c16985afcc8
yooveloper <yooveloper@gmail.com>
2023-06-29 15:17:14 +0900

    feat: storybook에서 tsconfig import alias로 절대경로 import 가능하게 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+const path = require('path');
+
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -8,6 +10,12 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    (config.resolve as any).alias = {
+      ['@/components']: path.resolve(__dirname, '../components'),
+    };
+    return config;
   },
 };
 export default config;
