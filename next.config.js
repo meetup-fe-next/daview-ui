faea41f800f20fa7cae5a793fe3abef39848ddd1
Jeesoo Min <kuongee@gmail.com>
2023-07-25 23:40:19 +0900

    feat: .all-contributorsrc 파일에서 프로필 가져오기

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
--- ui/mainPage/GithubProfile.tsx
+++ ui/mainPage/GithubProfile.tsx
@@ -1,5 +1,16 @@
+import { getContributors } from '@/server/libs/profileSDK';
+import Image from 'next/image';
+
 const GithubProfile = () => {
-  return <section className="absolute bottom-0 mb-[42px]">기여해주신 분들</section>;
+  const contributors = getContributors();
+
+  return (
+    <section className="absolute bottom-0 mb-[42px]">
+      {contributors.map(({ login, avatar_url }) => {
+        return <Image key={login} alt="profile" src={avatar_url} width="50" height="50" />;
+      })}
+    </section>
+  );
 };
 
 export default GithubProfile;
