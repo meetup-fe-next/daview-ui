50560d44cd6d1a0d1db3d20d1b0d8c56fb80993f
Jeesoo Min <kuongee@gmail.com>
2023-08-27 22:42:51 +0900

    feat: 기여하신 분 문구 추가

--- ui/mainPage/ContributorsProfile.tsx
+++ ui/mainPage/ContributorsProfile.tsx
@@ -6,12 +6,26 @@ import { ContributorsData } from '@/types/contributors.type';
 const ContributorsProfile = () => {
   const contributors: ContributorsData[] = use(getContributors());
 
+  if (contributors.length === 0) {
+    return <></>;
+  }
+
   return (
-    <section className="absolute bottom-0 mb-[42px] flex gap-x-3 [&>img]:rounded-[60px] [&>img]:border [&>img]:border-secondary-900">
-      {contributors.map(({ login, avatar_url }) => {
-        return <Image key={login} alt="profile" src={avatar_url} width="40" height="40" />;
-      })}
-    </section>
+    <div className="absolute bottom-0 mb-[42px]">
+      <Image
+        className="mb-[8px] ml-[18px]"
+        src="/images/contributorImage.svg"
+        width={125}
+        height={30}
+        quality={100}
+        alt="기여해주신 분들"
+      />
+      <section className="flex gap-x-3 [&>img]:rounded-[60px] [&>img]:border [&>img]:border-secondary-900">
+        {contributors.map(({ login, avatar_url }) => {
+          return <Image key={login} alt="profile" src={avatar_url} width="40" height="40" />;
+        })}
+      </section>
+    </div>
   );
 };
 
