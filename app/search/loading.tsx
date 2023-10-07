ad0bc9074132df4ccf0745f465af66a840f4d1a1
jun094 <jun094@naver.com>
2023-10-07 18:08:29 +0900

    feat: /search 페이지 loading page 추가

---
+++ app/search/loading.tsx
@@ -0,0 +1,12 @@
+import PageLayout from '@/components/PageLayout';
+import Header from '@/components/Header';
+
+export default function LoadingPage() {
+  return (
+    <>
+      <PageLayout.TopFixed>
+        <Header />
+      </PageLayout.TopFixed>
+    </>
+  );
+}
